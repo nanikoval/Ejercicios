11398d05e846a4f34de06bf0e82c05585413c0c3
nanikoval <48862252+nanikoval@users.noreply.github.com>
2019-06-24 21:10:36 -0300

    Add files via upload

--- testing_001/src/TestMascota.java
+++ testing_001/src/TestMascota.java
@@ -1,26 +1,32 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestMascota {
     private Mascota mascota;
 
-
+    @BeforeEach
+    public void setUp(){
+        mascota=new Mascota();
+    }
 
     @org.junit.jupiter.api.Test
     public void puedeJugar(){
-        Mascota mascota=new Mascota();
         mascota.setEstado(new Contenta());
         assertTrue(mascota.podesJugar());
-
     }
+
     @Test
     public void noPuedeJugar(){
-        Mascota mascota=new Mascota();
         mascota.setEstado(new Hambrienta());
         assertFalse(mascota.podesJugar());
+    }
+
+    @Test
+    public void podesJugarNull(){
+        mascota.setEstado(new Aburrida());
+        assertNull(mascota.podesJugar());
     }
 
 }
