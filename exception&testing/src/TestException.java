8e0fa0b4192a80a24e3d5001ff395208b0e2095a
nanikoval <48862252+nanikoval@users.noreply.github.com>
2019-07-01 13:28:08 -0300

    Add files via upload

--- exception&testing/src/TestException.java
+++ exception&testing/src/TestException.java
@@ -1,4 +1,5 @@
 import UserException.UserException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
@@ -6,11 +7,21 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestException {
     private Cuenta cuenta;
+
+    @BeforeEach
+    public void setUp(){
+        cuenta=new Cuenta();
+    }
+
     @Test
     public void retirarMontoNegativoError(){
-        cuenta=new Cuenta();
         assertThrows(UserException.class,()->cuenta.retirar(-100.0),
                 "La cantidad a retirar debe ser positiva");
     }
 
+    @Test
+    public void ingresarUnMontoNegativoError(){
+        assertThrows(UserException.class,()->cuenta.ingresar(-29.0),
+                "El monto a ingresar debe ser positivo");
+    }
 }
